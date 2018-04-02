5d5699ff5083df7d984e98b52f8f5264f447525c
Abhishek R <abhishek@RR.jed-i.lan>
2018-04-02 16:52:21 +0530

    script for copying files

---
+++ copy_files.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+# path for source and destination directories
+src_directory = "/home/user/source/"
+dst_directory = "/home/user/destination/"
+
+# list all files in src directory
+src_file_names = os.listdir(src_directory)
+
+print("Total Files in src directory : " + str(len(src_file_names)))
+
+# copy all files in src directory to destination directory with the same names
+for i in range(len(src_file_names)):
+    shutil.copy2(os.path.join(src_directory, src_file_names[i]), dst_directory)
