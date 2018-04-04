77f8498e1e44aa2d7fc915e0fb9c3a829bdc993e
AbhishekRS4 <abhi.apple4@gmail.com>
2018-04-04 13:16:38 +0530

    command line arguments parser

--- command_line_arg.py
+++ command_line_arg.py
@@ -6,24 +6,24 @@ import sys
 
 # parse and return dictionary of command-line args
 def get_args(argv):
-    options = {}
-    
-    while(argv):
-        if argv[0][0] == '-':
-            options[argv[0]] = argv[1]
-        argv = argv[1:]
+	options = {}
 
-    return options
+	while(argv):
+		if argv[0][0] == '-':
+			options[argv[0]] = argv[1]
+		argv = argv[1:]
+
+	return options
 
 def main():
-    myargs = get_args(sys.argv)
+	myargs = get_args(sys.argv)
 
-    if not(myargs):
-        # no command-line args
-        print("No command line args given")
-    else:
-        print(myargs)
-        print("")
+	if not(myargs):
+		# no command-line args
+		print("No command line args given")
+	else:
+		print(myargs)
+		print("")
 
 if __name__ == '__main__':
-    main()
+	main()
