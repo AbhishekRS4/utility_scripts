4fd317906f0dc1f824f695edff8fdf30a9bbfa08
AbhishekRS4 <abhi.apple4@gmail.com>
2018-10-04 17:46:33 +0530

    added commandline arguments

--- copy_files.py
+++ copy_files.py
@@ -3,17 +3,47 @@
 '''
 
 import os
+import sys
+import argparse
 import shutil
 
-# path for source and destination directories
-src_directory = "/home/user/source/"
-dst_directory = "/home/user/destination/"
 
-# list all files in src directory
-src_file_names = os.listdir(src_directory)
+def copy_files(src_dir, out_dir):
+    # list all files in src directory
+    src_files = os.listdir(src_dir)
 
-print("Total Files in src directory : " + str(len(src_file_names)))
+    # create the output directory if not present
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-# copy all files in src directory to destination directory with the same names
-for i in range(len(src_file_names)):
-	shutil.copy2(os.path.join(src_directory, src_file_names[i]), dst_directory)
+    print('Total Files in src directory : ' + str(len(src_files)))
+    print('Copying files started')
+
+    # copy all files in src directory to destination directory with the same names
+    for i in range(len(src_files)):
+        shutil.copy2(os.path.join(src_dir, src_files[i]), out_dir)
+
+    print('Copying files finished')
+
+
+def main():
+    # path for source and destination directories
+    src_dir = '/home/user/source/'
+    out_dir = '/home/user/destination/'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src_dir', default=src_dir,
+                        type=str, help='path to source directory')
+    parser.add_argument('-out_dir', default=out_dir,
+                        type=str, help='path to output directory')
+
+    input_args = vars(parser.parse_args(sys.argv[1:]))
+    for k in input_args.keys():
+        print(k + ': ' + str(input_args[k]))
+    print('')
+
+    copy_files(input_args['src_dir'], input_args['out_dir'])
+
+
+if __name__ == '__main__':
+    main()
