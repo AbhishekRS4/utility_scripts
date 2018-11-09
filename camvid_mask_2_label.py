66c897fa66ecaea1a0be87008080230a43e9d171
AbhishekRS4 <abhi.apple4@gmail.com>
2018-11-09 20:34:16 +0530

    updated camvid custom classes

--- camvid_mask_2_label.py
+++ camvid_mask_2_label.py
@@ -23,9 +23,39 @@ def mask_to_label(src_dir, out_dir):
     for mask_file in src_mask_files:
         mask = cv2.imread(os.path.join(src_dir, mask_file))
         temp_label = np.zeros(
-            (mask.shape[0], mask.shape[1], 1), dtype=np.uint8)
+            (mask.shape[0], mask.shape[1], 1), dtype=np.int32)
         temp_label[:, :, 0] = 1 * \
-            np.all((mask[:, :] == [128, 64, 128]), axis=2)
+            np.all((mask[:, :] == [192, 128, 0]), axis=2) # bicyclist
+        temp_label[:, :, 0] += 2 * \
+            np.all((mask[:, :] == [128,  0, 64]), axis=2) # car
+        temp_label[:, :, 0] += 3 * \
+            np.all((mask[:, :] == [64, 128, 192]), axis=2) # person
+        temp_label[:, :, 0] += 3 * \
+            np.all((mask[:, :] == [0,  64,  64]), axis=2) # person
+        temp_label[:, :, 0] += 4 * \
+            np.all((mask[:, :] == [192, 0,  128]), axis=2) # road
+        temp_label[:, :, 0] += 4 * \
+            np.all((mask[:, :] == [64,  0, 192]), axis=2) # road
+        temp_label[:, :, 0] += 4 * \
+            np.all((mask[:, :] == [128, 64, 128]), axis=2) # road
+        temp_label[:, :, 0] += 5 * \
+            np.all((mask[:, :] == [192,  0, 192]), axis=2) # two wheeler
+        temp_label[:, :, 0] += 6 * \
+            np.all((mask[:, :] == [192,  0, 0]), axis=2) # sidewalk
+        temp_label[:, :, 0] += 7 * \
+            np.all((mask[:, :] == [128, 128, 192]), axis=2) # sign
+        temp_label[:, :, 0] += 8 * \
+            np.all((mask[:, :] == [192, 128, 64]), axis=2) # truck
+        temp_label[:, :, 0] += 8 * \
+            np.all((mask[:, :] == [192, 128, 192]), axis=2) # truck
+        temp_label[:, :, 0] += 9 * \
+            np.all((mask[:, :] == [128, 64, 192]), axis=2) # train
+        temp_label[:, :, 0] += 10 * \
+            np.all((mask[:, :] == [64, 64, 0]), axis=2) # traffic light
+        temp_label[:, :, 0] += 11 * \
+            np.all((mask[:, :] == [64, 64, 128]), axis=2) # other moving
+        temp_label = temp_label.astype(np.uint8)
+         
         cv2.imwrite(os.path.join(out_dir, mask_file), temp_label)
 
     print('mask to label conversion completed')
