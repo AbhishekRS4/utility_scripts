1c91bb2395621b42623fa6dcc3b976c4344c51d1
AbhishekRS4 <abhi.apple4@gmail.com>
2019-02-07 14:56:34 +0530

    added commandline argument support, fixed issue when target directory does not exist

--- image_mask_overlay_generator.py
+++ image_mask_overlay_generator.py
@@ -2,34 +2,68 @@
 @author : Abhishek R S
 '''
 
+import sys
+import argparse
 import os
 import numpy as np
 import cv2
 
-# image, masks and output directories
-target_image_directory = "/home/user/target_image_directory/"
-target_mask_directory = "/home/user/target_mask_directory/"
-target_output_directory = "/home/user/target_output_directory/"
 
-# list of all images
-image_format = ".png"
-target_image_files = [x for x in os.listdir(target_image_directory) if x.endswith(image_format)]
-target_image_files.sort()
+def image_mask_overlay_generator(FLAGS):
+    src_image_dir = FLAGS.src_image_dir
+    src_mask_dir = FLAGS.src_mask_dir
+    tar_overlay_dir = FLAGS.tar_overlay_dir
+    img_format = FLAGS.img_format
+    alpha = FLAGS.alpha
 
-# alpha to control transparency of masks
-alpha = 0.2
+    if not os.path.exists(tar_overlay_dir):
+        os.makedirs(tar_overlay_dir)
 
-# overlay image and mask and save the output image
-for img_file in target_image_files:
-	image_path = os.path.join(target_image_directory, img_file)
-	mask_path = os.path.join(target_mask_directory, ("mask_" + img_file))
-	output_path = os.path.join(target_output_directory, ("overlay_" + img_file))  
+    # list of all images
+    target_image_files = [x for x in os.listdir(
+        src_image_dir) if x.endswith(img_format)]
+    target_image_files.sort()
 
- 	img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
-	mask = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2RGB)
+    # overlay image and mask and save the output image
+    for img_file in target_image_files:
+        image_path = os.path.join(src_image_dir, img_file)
+        mask_path = os.path.join(src_mask_dir, img_file)
+        output_path = os.path.join(tar_overlay_dir, img_file)
 
-	img_copy = img.copy()
-	img_mask_overlay = cv2.addWeighted(img_copy, 1, mask, alpha, 0, img)
- 	img_mask_overlay = cv2.cvtColor(img_mask_overlay, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+        mask = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2RGB)
 
-	cv2.imwrite(output_path, img_mask_overlay)
+        img_copy = img.copy()
+        img_mask_overlay = cv2.addWeighted(img_copy, 1, mask, alpha, 0, img)
+        img_mask_overlay = cv2.cvtColor(img_mask_overlay, cv2.COLOR_BGR2RGB)
+
+        cv2.imwrite(output_path, img_mask_overlay)
+
+
+def main():
+    # image, masks and output directories
+    # image format
+    # alpha, 0.0 - 1.0
+    src_image_dir = '/home/user/src_image_dir/'
+    src_mask_dir = '/home/user/src_mask_dir/'
+    tar_overlay_dir = '/home/user/tar_overlay_dir/'
+    img_format = 'png'
+    alpha = 0.2
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src_image_dir', default=src_image_dir,
+                        type=str, help='path to load images')
+    parser.add_argument('-src_mask_dir', default=src_mask_dir,
+                        type=str, help='path to load corresponding masks')
+    parser.add_argument('-tar_overlay_dir', default=tar_overlay_dir,
+                        type=str, help='path to save overlay images')
+    parser.add_argument('-img_format', default=img_format, type=str,
+                        choices=['jpg', 'png'], help='image format, png or jpg etc.')
+    parser.add_argument('-alpha', default=alpha, type=float,
+                        help='alpha (float 0.0-1.0) to control transparency of masks')
+
+    FLAGS, unparsed = parser.parse_known_args()
+    image_mask_overlay_generator(FLAGS)
+
+
+main()
